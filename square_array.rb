6c5c0f83d4bda6c508606206e4a62ffde7acb2db
Kyle Houghton <kylebenz@hotmail.com>
2018-12-21 00:12:10 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,13 +1,14 @@
 
 
-#def square_array(array)
-#  new_array = []
-#  array.each do |root|
-#    new_array.push(root ** 2)
-#  end
-#  new_array
-#end
-
 def square_array(array)
-  array.collect {|root| root ** 2}
+  new_array = []
+  array.each do |root|
+    new_array.push(root ** 2)
+  end
+  new_array
 end
+
+# Advanced Challenge:
+# def square_array(array)
+#   array.collect {|root| root ** 2}
+# end
